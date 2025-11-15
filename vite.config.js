370381ba28b835df5ff6e27da5cf4d3f35886107
VaheeMand <vladiman155@gmail.com>
2025-11-15 20:10:20 +0500

    Update README and App.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -12,5 +12,5 @@ export default defineConfig({
   ],
   server: {
     port: 3001,
-  }
+  },
 })
